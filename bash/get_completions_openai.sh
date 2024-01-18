bb45b6660cd3cb7184eb1978c24fe1ee3e3e8378
paul-rottger <paul.roettger@yahoo.de>
2024-01-18 14:32:10 +0100

    adding first results on jailbreaking experiments

---
+++ bash/get_completions_openai.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+REPO=$(git rev-parse --show-toplevel)
+
+source $REPO/env/bin/activate
+
+python ../src/2_get_completions_openai.py \
+    --gen_model "gpt-3.5-turbo-1106" \
+    --input_path $REPO/data/prompts/jailbreak_experiments_180124.csv \
+    --input_col "full_prompt" \
+    --caching_path $REPO/data/cache \
+    --output_path $REPO/data/completions/jailbreak_experiments_180124_gpt-3.5-turbo-1106.csv \
+    --n_batches 1 \
+    --start_batch 0 \
+    --max_workers 3 \
+    --n_samples 0
---
+++ src/1_generate_prompts.py
@@ -0,0 +1,49 @@
+import pandas as pd
+
+def generate_prompt_df(df_dict):
+
+    # print number of rows in each input dataframe
+    for key in df_dict.keys():
+        print(f"  {len(df_dict[key])} {key}")
+
+    # create a dataframe with all possible combinations of the input data
+    prompts_df = df_dict["prompt_templates"].copy()
+    for key in ["pct_propositions", "answer_options", "jailbreaks"]:
+        prompts_df = prompts_df.merge(df_dict[key], how="cross")
+
+    # create prompt column by applying the template to the input data
+    prompts_df["full_prompt"] = prompts_df.apply(lambda x: x["templ_prompt"].format(
+        pct_prompt=x["pct_prompt"],
+        ans_prompt=x["ans_prompt"],
+        jail_prompt=x["jail_prompt"] if x["jail_prompt"]==x["jail_prompt"] else "",
+    ), axis=1)
+
+    # print number of prompts generated
+    print("Generated {} prompts as a combination of all inputs.".format(len(prompts_df)))
+
+    return prompts_df
+
+
+def main():
+
+    # set input file paths
+    df_dict = {}
+    for input_file in ["prompt_templates", "pct_propositions", "answer_options", "jailbreaks"]:
+        df_dict[input_file] = pd.read_csv(f"../data/templates/{input_file}.csv".format(input_file))
+
+    # (optional) select specific templates and options for testing
+    # this is where 
+    df_dict["prompt_templates"] = df_dict["prompt_templates"][df_dict["prompt_templates"].templ_note=="implicit"]
+    df_dict["answer_options"] = df_dict["answer_options"][df_dict["answer_options"].ans_note=="main"]
+
+    # generate prompts
+    prompts_df = generate_prompt_df(df_dict)
+
+    # save prompts -- don't forget to rename the file!
+    prompts_df.to_csv("../data/prompts/test.csv", index=False)
+
+    return
+
+
+if __name__ == "__main__":
+    main()
